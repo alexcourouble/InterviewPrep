de5dbecabe4ab1dec995cbe82ced218a84232a5c
Alex Courouble <couralex6@gmail.com>
2017-09-21 10:08:15 -0400

    Added source to first problem

--- merge_two_sorted_list.py
+++ merge_two_sorted_list.py
@@ -1,11 +1,14 @@
+# Source: https://leetcode.com/problems/merge-two-sorted-lists/discuss/
+
 # Definition for singly-linked list.
 # class ListNode(object):
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
 
-#[2,4,6,8]
-#[1,3,5,7]
+# easy test case:
+# [2,4,6,8]
+# [1,3,5,7]
 
 class Solution(object):
     def mergeTwoLists(self, l1, l2):
